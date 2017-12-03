3717820a15f6d53d6fc1340e61cbd274918e1b36
Unknown <kasperhangard@hotmail.com>
2017-12-03 17:17:50 +0100

    Started integrating parts into full system

---
+++ EternalDarkessSurvival/Assets/Scripts/Character/CameraMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+
+    public Vector3 CameraOffsetVector;
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	    Camera.main.transform.position = Player.transform.position + CameraOffsetVector;
+	    Camera.main.transform.LookAt(Player.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    Camera.main.transform.position = Player.transform.position + CameraOffsetVector;
+
+	}
+}
--- EternalDarkessSurvival/Assets/Scripts/Character/ClickMovement.cs
+++ EternalDarkessSurvival/Assets/Scripts/Character/ClickMovement.cs
@@ -9,7 +9,7 @@ public class ClickMovement : MonoBehaviour
 {
 
     private NavMeshAgent _agent;
-	
+    private Vector3 targetPos;
 	
 	
 	// Use this for initialization
@@ -24,8 +24,18 @@ public class ClickMovement : MonoBehaviour
 		if (Input.GetMouseButtonDown(1))
 		{
             RaycastHit hit;
-		    if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-		    _agent.SetDestination(hit.point);
+		    if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+		        targetPos = hit.point;
+		        targetPos.y = transform.position.y;
+                _agent.SetDestination(targetPos);
+		    }
+            
 		}
-	}
+
+	    if ((transform.position - targetPos).magnitude < 1)
+	        _agent.isStopped = true;
+	    else
+	        _agent.isStopped = false;
+
+    }
 }
