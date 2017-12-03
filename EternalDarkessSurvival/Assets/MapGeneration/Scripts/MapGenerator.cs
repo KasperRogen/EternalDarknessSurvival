98636b7162a5d23b2cea2ff45453b40915dbfc13
Casper Thomsen <casper.thomsen100@gmail.com>
2017-12-03 20:47:00 +0100

    Auto stash before merge of "MapGen" and "origin/MapGen"

---
+++ EternalDarkessSurvival/Assets/MapGeneration/Scripts/MapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public float MapWidth = 400;
+	public float MapHeight = 400;
+
+	public const int SegmentWidthCount = 8;
+	public const int SegmentHeightCount = 8;
+
+	private float SegmentWidth, SegmentHeight;
+	private MapSegment[,] Segments;
+	
+	void initSegments(){
+		SegmentWidth = MapWidth / SegmentWidthCount;
+		SegmentHeight = MapHeight / SegmentHeightCount;
+
+		Segments = new MapSegment[SegmentWidthCount, SegmentHeightCount];
+
+		for(int i = 0; i < SegmentWidthCount; i++){
+			for(int j = 0; j < SegmentHeightCount; i++){
+				Segments[i, j] = new MapSegment(SegmentWidth * i + (SegmentWidth / 2), SegmentHeight * j + (SegmentHeight / 2), SegmentWidth, SegmentHeight);
+			}
+		}
+	}
+}
