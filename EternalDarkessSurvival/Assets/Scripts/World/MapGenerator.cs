657fbcf44a1c1f4755c294a1a0bf5abb9df8194b
Unknown <kasperhangard@hotmail.com>
2017-12-04 12:19:47 +0100

    Fixed Combat

--- EternalDarkessSurvival/Assets/Scripts/World/MapGenerator.cs
+++ EternalDarkessSurvival/Assets/Scripts/World/MapGenerator.cs
@@ -1,58 +1,62 @@
 ﻿using UnityEngine;
 
-public class MapGenerator : MonoBehaviour {
+namespace Assets.Scripts.World
+{
+    public class MapGenerator : MonoBehaviour {
 
-	public GenerateTerrainFromNoise TerrainGenerater;
+        public GenerateTerrainFromNoise TerrainGenerater;
 
-	public float MapWidth = 400;
-	public float MapHeight = 400;
+        public float MapWidth = 400;
+        public float MapHeight = 400;
 
-	public const int SegmentWidthCount = 8;
-	public const int SegmentHeightCount = 8;
+        public const int SegmentWidthCount = 8;
+        public const int SegmentHeightCount = 8;
 
-	private float SegmentWidth, SegmentHeight;
-	private MapSegment[,] Segments;
+        private float SegmentWidth, SegmentHeight;
+        private MapSegment[,] Segments;
 
-	public int Frequency = 3;
+        public int Frequency = 3;
 	
-	void Start()
-	{	
-		initSegments();
-		for(int i = 0; i < SegmentWidthCount-1; i++){
-			for(int j = 0; j < SegmentHeightCount-1; j++){
-				TerrainGenerater.GenerateTerrain(new Vector3(Segments[i,j].X, 0, Segments[i,j].Y), (int)SegmentWidth, (int)SegmentHeight, Segments[i,j].TerrainType);
-			}
-		}
-	}
+        void Start()
+        {	
+            initSegments();
+            for(int i = 0; i < SegmentWidthCount-1; i++){
+                for(int j = 0; j < SegmentHeightCount-1; j++){
+                    TerrainGenerater.GenerateTerrain(new Vector3(Segments[i,j].X, 0, Segments[i,j].Y), (int)SegmentWidth, (int)SegmentHeight, Segments[i,j].TerrainType);
+                }
+            }
+        }
 
-	// Algorithm
-	// Make counters for resource type fields
-	// Place Empty Camp Segment in the middle
-	// Go through all segments i, j
-	// 	Check [i-1,j]x, [i, j-1]x, [i+1, j]x, [i, j+1]x, [i+1, j+1]x, [i-1, j-1]x, [i+1, j-1], [i-1, j+1]; All empty => Make new resource
-	// 		Count resource up
-	//  else=> Do nothing (Maybe mountains, etc.) 
+        // Algorithm
+        // Make counters for resource type fields
+        // Place Empty Camp Segment in the middle
+        // Go through all segments i, j
+        // 	Check [i-1,j]x, [i, j-1]x, [i+1, j]x, [i, j+1]x, [i+1, j+1]x, [i-1, j-1]x, [i+1, j-1], [i-1, j+1]; All empty => Make new resource
+        // 		Count resource up
+        //  else=> Do nothing (Maybe mountains, etc.) 
 
-	void initSegments(){
-		SegmentWidth = MapWidth / SegmentWidthCount;
-		SegmentHeight = MapHeight / SegmentHeightCount;
+        void initSegments()
+        {
+            SegmentWidth = MapWidth / SegmentWidthCount;
+            SegmentHeight = MapHeight / SegmentHeightCount;
 
-		Segments = new MapSegment[SegmentWidthCount, SegmentHeightCount];
+            Segments = new MapSegment[SegmentWidthCount, SegmentHeightCount];
 
-		for(int i = 0; i < SegmentWidthCount-1; i++){
-			for(int j = 0; j < SegmentHeightCount-1; j++){
-				Segments[i, j] = new MapSegment(-MapWidth/2 + (SegmentWidth * i + (SegmentWidth / 2)), -MapHeight/2 + (SegmentHeight * j + (SegmentHeight / 2)), SegmentWidth, SegmentHeight);
+            for(int i = 0; i < SegmentWidthCount-1; i++){
+                for(int j = 0; j < SegmentHeightCount-1; j++){
+                    Segments[i, j] = new MapSegment(-MapWidth/2 + (SegmentWidth * i + (SegmentWidth / 2)), -MapHeight/2 + (SegmentHeight * j + (SegmentHeight / 2)), SegmentWidth, SegmentHeight);
 
-				if (i % Frequency == 0 && j % Frequency == 0)
-				{
-					Segments[i, j].TerrainType =
-						TerrainGenerater.GatherTypes[Random.Range(0, TerrainGenerater.GatherTypes.Count)].TerrainType;
-				}
-			}
-		}
+                    if (i % Frequency == 0 && j % Frequency == 0)
+                    {
+                        Segments[i, j].TerrainType =
+                            TerrainGenerater.GatherTypes[Random.Range(0, TerrainGenerater.GatherTypes.Count)].TerrainType;
+                    }
+                }
+            }
 		
-		Segments[SegmentWidthCount / 2, SegmentHeightCount / 2].TerrainType = PublicEnums.TerrainType.Spawn;
-	}
+            Segments[SegmentWidthCount / 2, SegmentHeightCount / 2].TerrainType = PublicEnums.TerrainType.Spawn;
+        }
 
 	
+    }
 }
